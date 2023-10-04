175294c833e96da1aa197f767c1447440d925ad1
Jerry <111554668+NotTheRussian@users.noreply.github.com>
2023-10-03 23:55:58 -0600

    Add files via upload

---
+++ TenaryOperator.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main (String[] args) {
+
+        String name = "John";
+        boolean isName = name == "Bob" ? true : false;
+        if (isName) {
+            System.out.println("Your name is bob!");
+        }
+        else {
+            System.out.println("Your name is not bob.");
+        }
+    }
+}
